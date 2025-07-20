9c802cadc08775ca5e91c85d69ec94b0b5d99669
RandomGamingDev <randomgamingdev@gmail.com>
2025-07-20 15:13:41 -0400

    fixed issues with firefox support

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: "https://www.nytimes.com/games/connections" });
-});
+if (typeof browser !== 'undefined') {
+  // Firefox (WebExtensions API)
+  browser.action.onClicked.addListener(() => {
+    browser.tabs.create({ url: "https://www.nytimes.com/games/connections" });
+  });
+} else if (typeof chrome !== 'undefined') {
+  // Chrome (Manifest V3)
+  chrome.action.onClicked.addListener(() => {
+    chrome.tabs.create({ url: "https://www.nytimes.com/games/connections" });
+  });
+} else {
+  console.error("Browser environment not detected for extension background script.");
+}
